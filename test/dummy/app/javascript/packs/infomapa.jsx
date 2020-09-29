fcd7eb808709ce7cfe629773ccd1b442afdfad1f
alejocruzrcc <alejocruzzz@gmail.com>
2020-09-29 17:03:15 -0500

    arregla selects, arregla reset arregla fecha y elimina inncesearios

--- test/dummy/app/javascript/packs/infomapa.jsx
+++ test/dummy/app/javascript/packs/infomapa.jsx
@@ -3,6 +3,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { render } from 'react-dom'
-//iimport App from '@pasosdejesus/mapa_tiempo_yi_liu'
+//import App from '@pasosdejesus/mapa_tiempo_yi_liu'
 import App from '../components/App'
 render(<App />, document.getElementById('root'))
